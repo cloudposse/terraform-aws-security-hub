5ca0697238d1087fa5a7a2e39c65f52d3c253cb9
Matt Calhoun <matt@cloudposse.com>
2020-11-11 17:12:41 -0500

    move docs to main module

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -7,17 +7,6 @@ variable "region" {
   description = "AWS region"
 }
 
-#----------------------------------------------------------------------------------------------------------------------
-# A list of standards/rulesets to enable
-#
-# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/securityhub_standards_subscription#argument-reference
-#
-# The possible values are:
-#
-#  - standards/aws-foundational-security-best-practices/v/1.0.0
-#  - ruleset/cis-aws-foundations-benchmark/v/1.2.0
-#  - standards/pci-dss/v/3.2.1
-#----------------------------------------------------------------------------------------------------------------------
 variable "enabled_standards" {
   description = "A list of standards to enable in the account"
   type        = list(string)
--- variables.tf
+++ variables.tf
@@ -2,6 +2,18 @@
 # OPTIONAL PARAMETERS
 # These variables have defaults set that can be overridden for desired behavior.
 #-----------------------------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------------------------
+# A list of standards/rulesets to enable
+#
+# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/securityhub_standards_subscription#argument-reference
+#
+# The possible values are:
+#
+#  - standards/aws-foundational-security-best-practices/v/1.0.0
+#  - ruleset/cis-aws-foundations-benchmark/v/1.2.0
+#  - standards/pci-dss/v/3.2.1
+#----------------------------------------------------------------------------------------------------------------------
 variable "enabled_standards" {
   description = "A list of standards to enable in the account"
   type        = list
