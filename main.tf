4bda0e04eeb3a58bfbe7502bef15477327afbead
Matt Calhoun <matt@cloudposse.com>
2021-05-05 10:43:32 -0400

    bump module versions

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_securityhub_standards_subscription" "this" {
 #-----------------------------------------------------------------------------------------------------------------------
 module "sns_topic" {
   source  = "cloudposse/sns-topic/aws"
-  version = "0.11.0"
+  version = "0.16.0"
   count   = local.create_sns_topic ? 1 : 0
 
   attributes      = ["securityhub"]
@@ -36,7 +36,7 @@ module "sns_topic" {
 
 module "imported_findings_label" {
   source  = "cloudposse/label/null"
-  version = "0.22.1"
+  version = "0.24.1"
 
   attributes = ["securityhub-imported-findings"]
   context    = module.this.context
