0306025d946111b6367f27ab66bfe100736157d6
Yonatan Koren <me@yonatankoren.com>
2021-06-04 09:44:33 -0400

    Split up mian.tf since more resources now exist; create KMS key for SNS topic and allow EventBridge the required permissions for it to write to the encrypted topic, using Resource-based policies; change the service allowed to publish to the SNS topic to events.amazonaws.com from cloudwatch.amazonaws.com (the latter service is not the one identifying EventBridge aka CloudWatchEvents).

--- main.tf
+++ main.tf
@@ -6,64 +6,13 @@ resource "aws_securityhub_account" "this" {
 }
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Optionally subscribe to Security Hub Standards 
+# Optionally subscribe to Security Hub Standards
 # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards.html
 #-----------------------------------------------------------------------------------------------------------------------
 resource "aws_securityhub_standards_subscription" "this" {
   for_each      = local.enabled_standards_arns
   depends_on    = [aws_securityhub_account.this]
   standards_arn = each.key
-}
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Optionally configure Event Bridge Rules and SNS subscriptions 
-# https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-cwe-integration-types.html
-# https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/resource-based-policies-cwe.html#sns-permissions
-#-----------------------------------------------------------------------------------------------------------------------
-module "sns_topic" {
-  source  = "cloudposse/sns-topic/aws"
-  version = "0.16.0"
-  count   = local.create_sns_topic ? 1 : 0
-
-  attributes      = ["securityhub"]
-  subscribers     = var.subscribers
-  sqs_dlq_enabled = false
-
-  allowed_aws_services_for_sns_published = ["cloudwatch.amazonaws.com"]
-
-  context = module.this.context
-}
-
-module "imported_findings_label" {
-  source  = "cloudposse/label/null"
-  version = "0.24.1"
-
-  attributes = ["securityhub-imported-findings"]
-  context    = module.this.context
-}
-
-resource "aws_cloudwatch_event_rule" "imported_findings" {
-  count       = local.enable_notifications == true ? 1 : 0
-  name        = module.imported_findings_label.id
-  description = "SecurityHubEvent - Imported Findings"
-  tags        = module.this.tags
-
-  event_pattern = jsonencode(
-    {
-      "source" : [
-        "aws.securityhub"
-      ],
-      "detail-type" : [
-        var.cloudwatch_event_rule_pattern_detail_type
-      ]
-    }
-  )
-}
-
-resource "aws_cloudwatch_event_target" "imported_findings" {
-  count = local.enable_notifications == true ? 1 : 0
-  rule  = aws_cloudwatch_event_rule.imported_findings[0].name
-  arn   = local.imported_findings_notification_arn
 }
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -79,5 +28,6 @@ locals {
   ])
 }
 
+data "aws_caller_identity" "this" {}
 data "aws_partition" "this" {}
 data "aws_region" "this" {}
