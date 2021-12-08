fe1db6660800e3b6f4fa3633e9549a924b9dacfd
milldr <miller0daniel@gmail.com>
2021-12-08 12:15:27 -0800

    update cloudposse module versions

--- eventbridge.tf
+++ eventbridge.tf
@@ -16,7 +16,7 @@ module "sns_kms_key_label" {
 
 module "sns_kms_key" {
   source  = "cloudposse/kms-key/aws"
-  version = "0.10.0"
+  version = "0.12.1"
   count   = local.create_sns_topic ? 1 : 0
 
   name                = local.create_sns_topic ? module.sns_kms_key_label[0].id : ""
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "sns_kms_key_policy" {
 
 module "sns_topic" {
   source  = "cloudposse/sns-topic/aws"
-  version = "0.16.0"
+  version = "0.20.1"
   count   = local.create_sns_topic ? 1 : 0
 
   attributes        = ["securityhub"]
