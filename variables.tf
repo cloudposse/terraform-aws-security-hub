df7c3964d94bcdb5a464d4e44cc6ed8595b7d75b
Alex Boyd <alex@minware.com>
2024-04-21 21:42:48 -0400

    Fix doc comment on control_finding_generator

--- variables.tf
+++ variables.tf
@@ -10,14 +10,16 @@ variable "enable_default_standards" {
 }
 
 variable "control_finding_generator" {
-  description = "Updates whether the calling account has consolidated control findings turned on.
+  description = <<-DOC
+  Updates whether the calling account has consolidated control findings turned on.
   If the value for this field is set to ,
-  
+
   SECURITY_CONTROL - Security Hub generates a single finding for a control check even when
   the check applies to multiple enabled standards.
 
-  STANDARD_CONTROL - Security Hub generates separate findings for a control check when the 
-  check applies to multiple enabled standards."
+  STANDARD_CONTROL - Security Hub generates separate findings for a control check when the
+  check applies to multiple enabled standards.
+  DOC
   type        = string
   default     = "SECURITY_CONTROL"
 }
